c2a12a25f0eb9d112abbb46643bb0526fcf0da98
hun <algum1737@gmail.com>
2020-06-03 17:28:30 +0900

    3번째 넣기

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
-export default (req, res) => {
-    res.send('hihierdf')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('here is root' + Math.random())
+})
+app.use('/hello', require('./hello'))
+
+module.exports = {
+    path: '/api',
+    handler: app
 }
